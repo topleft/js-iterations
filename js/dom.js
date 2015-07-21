e671dbbbccd266099ca90c7adc92f6d381689c20
topleft <pete.james.jeffryes@gmail.com>
2015-07-21 17:49:22 -0600

    all number functions are posting to page via DOM

--- js/dom.js
+++ js/dom.js
@@ -9,3 +9,59 @@ Hints for the first problem:
     - takes the value returned by the `sum` function and displays it on the page
 
 */
+
+
+//////////////////SUM ALL
+
+//target options
+
+var options = document.getElementsByTagName("option");
+
+// sum option values using sum(options)
+// use return value and append it to the dom
+  // create span element and append to div approriate div of HTML
+  //fill in span innerText with sum result
+
+// var resultsContainer = document.getElementById("results");
+
+///////////////////SUM ALL
+
+document.getElementById("sum-all").addEventListener("click", function(event){
+    document.getElementById("results").innerHTML =
+      "<h4>Sum of All</h4>"+"$"+sumAll(options).toFixed(2);
+});
+
+///////////////////SUM SELECTED
+
+document.getElementById("sum-selected").addEventListener("click", function(event){
+    document.getElementById("results").innerHTML =
+      "<h4>Sum of Selected</h4>"+"$"+sumSelected(options).toFixed(2);
+});
+
+////////////////////AVG ALL
+
+document.getElementById("average-all").addEventListener("click", function(event){
+    document.getElementById("results").innerHTML =
+      "<h4>Average of All</h4>"+"$"+averageAll(options).toFixed(2);
+});
+
+////////////////////AVG SELECTED
+
+
+document.getElementById("average-selected").addEventListener("click", function(event){
+    document.getElementById("results").innerHTML =
+      "<h4>Average of Selected</h4>"+"$"+averageSelected(options).toFixed(2);
+});
+
+///////////////////COUNT
+
+document.getElementById("count-selected").addEventListener("click", function(event) {
+    document.getElementById("results").innerHTML =
+    "<h4>Count of Selected</h4>"+countSelected(options);
+});
+
+
+
+
+
+
--- js/logic.js
+++ js/logic.js
@@ -10,3 +10,53 @@ Hints for the first problem:
 NOTE: use a `for` loop to make this happen.
 
 */
+
+function sumAll(options){
+  var sum = 0;
+  for (var i = 0; i < options.length; i++) {
+      sum += parseInt(options[i].value);
+  }
+  return sum;
+}
+
+function countSelected(options){
+    var total = 0;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected){
+        total ++;
+    }
+  }
+  return total;
+
+}
+
+function sumSelected(options){
+  var sum = 0;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        sum += parseInt(options[i].value);
+    }
+  }
+  return sum;
+}
+
+function averageAll(options) {
+  var sum = 0;
+    for (var i = 0; i < options.length; i++) {
+      sum += parseInt(options[i].value);
+}
+  return sum/options.length;
+}
+
+function averageSelected(options){
+  var sum = 0;
+  var totalSelected = 0;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected){
+      sum += parseInt(options[i].value);
+      totalSelected++;
+    }
+  }
+  return sum/totalSelected;
+}
+
